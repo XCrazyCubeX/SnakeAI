0d1dc5261a4c0f8800b3170a41566c28d03213d6
AnonT <officialjoy04@gmail.com>
2025-08-12 11:16:53 +0200

    Added all states to __init__, gave every function a docstring for function info and to see what it will be returning in the future

--- training/env.py
+++ training/env.py
@@ -12,7 +12,6 @@ pygame.init()
 # Frame name
 pygame.display.set_caption('Flappy Bird')
 
-
 ####################
 # Global Variables
 ####################
@@ -36,9 +35,9 @@ TEXT        = (230, 230, 235)
 ACCENT      = (120, 200, 130)
 
 # Observation Shape
-N_CHANNELS = 3
-HEIGHT = GRID_H
-WIDTH = GRID_W
+N_CHANNELS  = 3
+HEIGHT      = GRID_H
+WIDTH       = GRID_W
 
 
 
@@ -53,22 +52,51 @@ WIDTH = GRID_W
 ##########################
 
 class Snake(gym.Env):
+    """
+    Snake environment met pixel-observations (obs).
+    Each env.step(action) moves the snake exactly 1 cell.
+    """
     metadata = {'render.modes': ['human']}
 
-    # Initialize flappy bird game, make sure all self values exist
-    # Here we also define our observation space and action space
+    # actions: 0=Up, 1=Right, 2=Down, 3=Left
+    _DIRS = np.array([(0, -1), (1, 0), (0, 1), (-1, 0)])
+    _OPPOSITE = {0: 2, 2: 0, 1: 3, 3: 1}
 
-    def __init__(self):
+
+
+    def __init__(self, grid_w=GRID_W, grid_h=GRID_H, wrap=False):
+        """
+        # Initialize flappy bird game, make sure all self values exist
+        # Here we also define our observation space and action space
+        """
         super(Snake, self).__init__()
+
+        self.W = int(grid_w)
+        self.H = int(grid_h)
+        self.wrap = bool(wrap)
+
 
         # Define action space
         self.action_space = spaces.Discrete(4)  # up, right, down, left
-
-        self.screen = pygame.display.set_mode(())
-
         # Observation space
         self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(0,), dtype=np.int32)
+                                            shape=(0,), dtype=np.int8)
+
+        # internal state, snake food, score...
+
+        self.snake = None           # list of (x,y), head = snake[0]
+        self.direction = 1          # start right
+        self.food = None
+        self.score = 0
+        self._rng = None
+        self._last_obs = None
+
+        # some pygame stuff
+        # only used when rendering
+        self._screen = None
+        self._surface = None
+        self._pygame_inited = False
+
 
 
 
@@ -76,6 +104,12 @@ class Snake(gym.Env):
     # Add more observations!!
 
     def _get_observation(self):
+        """
+        this is where the observations will be made
+        :return: obs
+        """
+
+        # obs = np.array([])
         ...
 
 
@@ -86,6 +120,11 @@ class Snake(gym.Env):
     # Frame by frame
 
     def step(self, action):
+        """
+        every frame of the game/every action taken
+        :param action:
+        :return: obs, reward, terminated, truncated, info
+        """
         ...
 
 
@@ -95,8 +134,14 @@ class Snake(gym.Env):
     # Resets everything
 
     def reset(self, seed=None, options=None):
-        ...
+        """
 
+        :param seed:
+        :param options:
+        :return: return self._get_observation(), self.info
+
+        """
+        ...
 
 
 
@@ -104,6 +149,10 @@ class Snake(gym.Env):
     # User interface User experience
 
     def render(self, mode='human'):
+        """
+        actually just renders everything, nothing special
+
+        """
         ...
 
 
@@ -112,5 +161,18 @@ class Snake(gym.Env):
     # Using function inside the step function
     # Keeps checking for rewards
 
-    def reward_value(self):
+    def _get_rewards(self) -> tuple[int, bool, bool]:
+        """
+        # A function that gives out rewards or punishments
+        #
+        # :return: reward, terminated, truncated
+        """
+        # reward = 0
+        # terminated = False
+        # truncated = False
+        # ...
+        # ...
+        # ...
+        # return reward, terminated, truncated
         ...
+
